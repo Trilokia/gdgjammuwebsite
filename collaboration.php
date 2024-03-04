49f87ee15d21de48259f3f8c74823ce0989e4cf7
Trilokia <trilokia.abhinandan.10@gmail.com>
2024-03-04 22:24:32 +0530

    add gif

--- collaboration.php
+++ collaboration.php
@@ -54,7 +54,7 @@ include 'header.php';
 
         <section class="hero">
         <div class="hero-img">
-            <img src="images/colloboration.gif" alt="graphics" style="margin-left:auto;
+            <img src="images/collob.gif" alt="graphics" style="margin-left:auto;
             margin-right: auto;" />
           </div>
           <div class="hero-text">
